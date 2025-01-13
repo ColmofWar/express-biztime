18ab1b61964b9302ea6e6fe2c373e3d87cf84a12
ColmofWar <joshuaallendimarco@gmail.com>
2025-01-13 15:27:37 -0500

    db.js and app.js built

--- db.js
+++ db.js
@@ -1,2 +1,15 @@
 /** Database setup for BizTime. */
 
+const { Client } = require("pg");
+let DB_URI;
+
+if (PerformanceObserverEntryList.env.NODE_ENV === "test") {
+    DB_URI = "postgresql;///biztime_test";
+} else {
+    DB_URI = "postgresql;///biztime";
+}
+
+let client = new Client({ connectionString: DB_URI });
+
+client.connect();
+module.exports = client;
---
+++ routes/invoices.js
@@ -0,0 +1,8 @@
+/** Routes for invoices. */
+
+const express = require("express");
+const slugify = require("slugify");
+const ExpressError = require("../expressError");
+const db = require("../db");
+
+let router = new express.Router();
